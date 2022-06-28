53bf5351ce9cd67951b65a60fa06dca6fd256b80
Vuong Ly <imvuong@users.noreply.github.com>
2022-06-27 22:04:44 -0400

    Add tutorials

---
+++ 05-python-data-types.py
@@ -0,0 +1,34 @@
+# https://www.w3schools.com/python/python_datatypes.asp
+
+x = 5
+print(type(x))
+
+x = "Hello World"
+
+x = 20
+
+x = 20.5
+
+x = 1j
+
+x = ["apple", "banana", "cherry"]
+
+x = ("apple", "banana", "cherry")
+
+x = range(6)
+
+x = {"name" : "John", "age" : 36}
+
+x = {"apple", "banana", "cherry"}
+
+x = frozenset({"apple", "banana", "cherry"})
+
+x = True
+
+x = b"Hello"
+
+x = bytearray(5)
+
+x = memoryview(bytes(5))
+
+x = None
